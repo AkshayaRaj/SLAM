7f397f1d49e7692ed37705ca237f543f119c1f65
Akshay Raj Dayal <ard.astro@gmail.com>
2019-10-21 19:14:51 +0530

    normalized coords

--- extractor.py
+++ extractor.py
@@ -3,14 +3,31 @@ import numpy as np
 from skimage.transform import FundamentalMatrixTransform
 from skimage.measure import ransac
 
+np.set_printoptions(suppress=True)
+
+# turn [[x,y]] -> [[x,y,1]]
+def add_ones(x):
+    return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
 class Extractor(object):
     GX = 16//2
     GY = 12//2
 
-    def __init__(self):
+    def __init__(self, K):
         self.orb = cv2.ORB_create()
         self.last = None
         self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+        self.K = K
+        self.Kinv = np.linalg.inv(self.K)
+
+    def normalize(self,pts):
+        return np.dot(self.Kinv, add_ones(pts).T).T[:, 0:2]
+        
+    def denormalize(self,  pt): 
+        #print(self.Kinv
+        ret = np.dot(self.K, np.array([pt[0],pt[1],1]))
+        #ret /= ret[2]
+        return int(round(ret[0])),int(round(ret[1]))
 
     def extract(self,img):
         # detection
@@ -39,6 +56,11 @@ class Extractor(object):
             #print('yes')
             ret = np.array(ret)
             #print(ret.shape)
+            #normalize coordinates; subtract to move to 0
+            ret[:, 0, :] = self.normalize(ret[:, 0, :])
+            ret[:, 1, :] = self.normalize(ret[:, 1, :] )
+    
+
             model, inliers = ransac((ret[:, 0], ret[:, 1]),
                                     FundamentalMatrixTransform,
                                     min_samples = 8,
@@ -47,6 +69,8 @@ class Extractor(object):
             )
             #print(sum(inliers))
             ret = ret[inliers] 
+            s,v,d = np.linalg.svd(model.params) 
+            print(v)
             
 
 
