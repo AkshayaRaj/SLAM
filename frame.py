48e13740884036a422ce4c0b8948ac874350dabc
Akshay Raj Dayal <ard.astro@gmail.com>
2019-11-19 21:11:34 +0530

    refactor to keep indices

--- frame.py
+++ frame.py
@@ -6,15 +6,20 @@ from skimage.measure import ransac
 
 np.set_printoptions(suppress=True)
 
+IRt = np.zeros((4,4)) 
+IRt= np.eye(4)
+
 # turn [[x,y]] -> [[x,y,1]]
 def add_ones(x):
     return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
-def extractRt(E):
+# pose
+def extractPose(E):
     
     # compute R and t from Essential Matrix:
     W = np.mat([[0,-1,0],[1,0,0],[0,0,1]],dtype=float)
     U, d, Vt = np.linalg.svd(E) 
+    #print(d.shape)
     assert np.linalg.det(U) > 0
     if np.linalg.det(Vt) < -0:
         Vt *= -1.0
@@ -23,15 +28,18 @@ def extractRt(E):
         R = np.dot(np.dot(U, W.T), Vt)
     t = U[:, 2]
     #print(R,t)
-    Rt = np.concatenate([R,t.reshape(3,1)], axis=1)
-    return Rt
+    ret = np.eye(4)
+    ret[:3, :3] = R
+    ret[:3, 3] = t
+    
+    return ret
 
 def extract(img):
     orb = cv2.ORB_create()
     # detection
     pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
     # extraction
-    kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in pts]
+    kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in pts] 
     kps,des = orb.compute(img,kps)
 
     # return pts and des
@@ -45,23 +53,29 @@ def denormalize(K, pt):
     ret /= ret[2]
     return int(round(ret[0])),int(round(ret[1]))
 
-def match(f1, f2):
+def match_frames(f1, f2):
     bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     matches = bf.knnMatch(f1.des,f2.des, k=2)
-
+    
     # Lowe's ratio test
     ret = []
+    idx1, idx2 = [], []
     for m,n in matches:
         if m.distance < 0.75 * n.distance:
+            # keep around indices
+            idx1.append(m.queryIdx)
+            idx2.append(m.trainIdx)
             p1 = f1.pts[m.queryIdx]
             p2 = f2.pts[m.trainIdx]
             ret.append((p1,p2))  
     assert len(ret) >= 8
 
     ret = np.array(ret)
+    idx1 = np.array(idx1)
+    idx2 = np.array(idx2)  
 
     # fit matrix
-    model, inliers = ransac((ret[:, 0], ret[:, 1]),
+    model, inliers = ransac((ret[:, 0], ret[:, 1]), # inliners is a boolean matrix of True and False
                                         #FundamentalMatrixTransform,
                                         EssentialMatrixTransform,
                                         min_samples = 8,
@@ -69,14 +83,14 @@ def match(f1, f2):
                                         #residual_threshold = 1,
                                         max_trials = 200
                 )
-    print(sum(inliers), len(inliers))
-
+    
+    # print(sum(inliers), len(inliers))
     # ignore outliers
     ret = ret[inliers] 
-    Rt = extractRt(model.params)
+    Rt = extractPose(model.params)
 
     # return 
-    return ret, Rt
+    return idx1[inliers], idx2[inliers], Rt
 
 
 
@@ -84,6 +98,7 @@ class Frame(object):
     def __init__(self, img, K):
         self.K = K
         self.Kinv = np.linalg.inv(self.K)
+        self.pose = IRt
 
         pts, self.des = extract(img)
         self.pts = normalize(self.Kinv, pts)
